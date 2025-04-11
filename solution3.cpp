61d6e91d977fcb2f9302545d00e38a98ef5b449a
Madhu Shankara G S <98076468+madhushankara@users.noreply.github.com>
2025-04-11 10:46:42 +0530

    Update solution3.cpp

--- solution3.cpp
+++ solution3.cpp
@@ -2,21 +2,23 @@
 using namespace std;
 
 int longestValidParentheses(string s) {
-    int maxLength = 0;
-    stack<int> st;
-    st.push(-1);  
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '(') {
+int maxLength = 0;
+stack<int> st;
+st.push(-1);  
+for (int i = 0; i < s.length(); i++) {
+    if (s[i] == '(') {
+        st.push(i);
+    } 
+    else {
+        st.pop();
+        if (st.empty()) {
             st.push(i);
-        } else {
-            st.pop();
-            if (st.empty()) {
-                st.push(i);
-            } else {
-                maxLength = max(maxLength, i - st.top());
-            }
+        } 
+        else {
+            maxLength = max(maxLength, i - st.top());
         }
     }
+}
     return maxLength;
 }
 
