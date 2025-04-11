b000559f92a1d8122c24a1447e7ff74ae2221f7f
Madhu Shankara G S <98076468+madhushankara@users.noreply.github.com>
2025-04-11 10:32:17 +0530

    Update solution2.cpp

--- solution2.cpp
+++ solution2.cpp
@@ -6,11 +6,9 @@ struct ListNode {
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
-
 bool hasCycle(ListNode *head) {
     ListNode *nospeed = head;
     ListNode *speed = head;
-    
     while (speed != NULL && speed->next != NULL) {
         nospeed = nospeed->next;      
         speed = speed->next->next;   
@@ -22,16 +20,12 @@ bool hasCycle(ListNode *head) {
 }
 
 int main() {
-
     ListNode *head = new ListNode(1);
     ListNode *node3 = new ListNode(3);
     ListNode *node4 = new ListNode(4);
-    
     head->next = node3;     
     node3->next = node4;    
     node4->next = node3;      
-    
     cout << (hasCycle(head) ? "true" : "false") << endl;
-
     return 0;
 }
