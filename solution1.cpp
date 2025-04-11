e075138dd5d51ac78f26a3b0d245a5fd0defee01
Madhu Shankara G S <98076468+madhushankara@users.noreply.github.com>
2025-04-11 10:21:29 +0530

    Update solution1.cpp

--- solution1.cpp
+++ solution1.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 int main() {
-    int arr[] = {1, 2, 3, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] + 1 != arr[i + 1]) {
-            cout << arr[i] + 1 << endl;
-            break;
-        }
+    int nums[] = {1, 2, 3, 5};
+    int n = sizeof(nums) / sizeof(nums[0]);
+    int result = n;
+    for (int i = 0; i < n; i++) {
+        result ^= i ^ nums[i];
     }
-    return 0;
+    return result; 
 }
