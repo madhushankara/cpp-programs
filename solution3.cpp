b3655028d573ce6fcac8319b95085a6eb9e8af44
Madhu Shankara G S <98076468+madhushankara@users.noreply.github.com>
2025-04-11 10:43:28 +0530

    Create solution3.cpp

---
+++ solution3.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestValidParentheses(string s) {
+    int maxLength = 0;
+    stack<int> st;
+    st.push(-1);  
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            st.push(i);
+        } else {
+            st.pop();
+            if (st.empty()) {
+                st.push(i);
+            } else {
+                maxLength = max(maxLength, i - st.top());
+            }
+        }
+    }
+    return maxLength;
+}
+
+int main() {
+    string s = "((()";
+    cout << longestValidParentheses(s) << endl;
+    return 0;
+}
